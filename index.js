872e66d614df088446f62a596d54413869673827
PrantaDas <prantodas043@gmail.com>
2022-05-05 16:22:06 +0600

    api end point created

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World from server');
+})
+
+
+app.listen(port, () => {
+    console.log('listenin to the port', port);
+})
