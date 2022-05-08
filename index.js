f7ae6ec142dc8181afe1061ec3f796d62d6e7ace
PrantaDas <prantodas043@gmail.com>
2022-05-08 22:00:52 +0600

    search by email api added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -22,6 +23,51 @@ async function run() {
 
         const productCollection = client.db('dailydeals').collection('products');
 
+
+
+        function verifyJWT(req, res, next) {
+            const authHeader = req.headers.authorization;
+            if (!authHeader) {
+                return res.status(401).send({ message: 'unauthorized access' })
+            }
+            const token = authHeader.split(' ')[1];
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+                if (err) {
+                    return res.status(403).send({
+                        message
+                            : 'forbidden access'
+                    })
+                }
+                console.log('decoded', decoded);
+                req.decoded = decoded;
+            })
+            console.log('inside jwt',authHeader);
+            next();
+        }
+
+        app.get('/myitems', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
+            const email = req.query.email;
+            console.log(email);
+            const items = { email: email };
+                const cursor = productCollection.find(items);
+                const result = await cursor.toArray();
+                res.send(result);
+            if (email === decodedEmail) {
+                const items = { email: email };
+                const cursor = productCollection.find(items);
+                const result = await cursor.toArray();
+                res.send(result);
+            }
+            else{
+                res.status(403).send(({message:'forbidden access'}))
+            }
+            
+            console.log(result);
+
+        })
+
+
         // load all products
 
         app.get('/products', async (req, res) => {
@@ -39,6 +85,16 @@ async function run() {
             const query = { _id: ObjectId(id) };
             const result = await productCollection.findOne(query);
             res.send(result);
+        })
+
+        // login api 
+
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            });
+            res.send({ accessToken });
         })
 
         // update quantity
@@ -77,6 +133,18 @@ async function run() {
             const result = await productCollection.insertOne(newProduct);
             res.send(result);
         })
+
+        // search by query using email
+
+
+
+        // app.get('/products', async (req, res) => {
+        //     const items= { "name":"John" };
+        //     const cursor = productCollection.find(items);
+        //     const result = await cursor.toArray();
+        //     res.send(result)
+        // })
+
 
 
     }
